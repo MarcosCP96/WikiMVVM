71ef3a48f90243980290165f5f778f06cc4c6c00
u01a5135 <u01a5135@lacaixa.es>
2022-11-17 11:08:08 +0100

    añadido boton para conseguir 10 articulos aleatorios nuevos

--- app/src/main/java/com/example/wikimvvm/views/ArticleFragment.kt
+++ app/src/main/java/com/example/wikimvvm/views/ArticleFragment.kt
@@ -19,13 +19,13 @@ class ArticleFragment : Fragment() {
     private var _binding:FragmentArticleBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentArticleBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        _binding = FragmentArticleBinding.inflate(inflater, container, false)
+//        return binding.root
+//    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/com/example/wikimvvm/views/ArticleListFragment.kt
+++ app/src/main/java/com/example/wikimvvm/views/ArticleListFragment.kt
@@ -5,16 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wikimvvm.R
+import com.example.wikimvvm.databinding.FragmentArticleBinding
 import com.example.wikimvvm.model.ArticleAdapter
 import com.example.wikimvvm.model.ArticleResponse
 import com.example.wikimvvm.repository.ArticleRepository
 
 class ArticleListFragment : Fragment() {
     private val articleRepository = ArticleRepository
-    val listOfArticles = mutableListOf<ArticleResponse>()
+    private var _binding: FragmentArticleBinding? = null
+    private val binding get() = _binding!!
+
+    private val listOfArticles = mutableListOf<ArticleResponse>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +28,14 @@ class ArticleListFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_article_list, container, false)
         repeat(10){
             listOfArticles.add(articleRepository.getRandomArticle())
+        }
+        val randomButton = view.findViewById<Button>(R.id.randomButton).setOnClickListener {
+            listOfArticles.clear()
+            repeat(10){
+                listOfArticles.add(articleRepository.getRandomArticle())
+            }
+            val recyclerView = view.findViewById<RecyclerView>(R.id.articleList)
+            recyclerView.adapter = ArticleAdapter(listOfArticles)
         }
         return view
     }
